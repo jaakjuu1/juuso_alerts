0a7f4acb1d3230cec20be90ec70c27e8a24959c6
JNE JKL <juuso.jaakkola@gmail.com>
2021-03-04 20:56:34 +0200

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const alert = require('./index.js');
-
-alert();
-alert({type: `info`, msg: `Kiia is awesome, beautiful, authentic!`, name: `HEAVENLY`});
-alert({type: `warning`, msg: `Henna has some extraordinary skills!`, name: `BASE`});
-alert({type: `error`, msg: `Do not get overly confident about you!`});
-alert({type: `success`, msg: `Be honest, humble and share your deep thoughts and feelings!`, name: `DONE`})
